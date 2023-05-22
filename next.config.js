3d4271e690ff0364dc9a3315b7c431b60dae08ec
Robin Saini <robinsaini@technogetic.com>
2023-05-22 11:20:00 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ["fakestoreapi.com"],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '*',
+            port: '',
+            pathname: '*',
+          },
+        ],
+    }
+}
 
 module.exports = nextConfig
